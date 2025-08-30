893ed03ed119e00683c6dfe910149a88b692e5b3
Robert Rumney <robertrumney@gmail.com>
2025-08-30 13:49:48 +0200

    Update WordLimitEnforcer.cs

--- WordLimitEnforcer.cs
+++ WordLimitEnforcer.cs
@@ -1,26 +1,62 @@
 using UnityEngine;
 using UnityEngine.UI;
+#if TMP_PRESENT
+using TMPro;
+#endif
 
-[RequireComponent(typeof(InputField))]
 public class WordLimitEnforcer : MonoBehaviour
 {
     public int wordLimit = 10;
-    private InputField inputField;
+    public InputField legacyInput;
+#if TMP_PRESENT
+    public TMP_InputField tmpInput;
+#endif
 
-    private void Awake()
+    void Awake()
     {
-        inputField = GetComponent<InputField>();
-        inputField.onValueChanged.AddListener(EnforceWordLimit);
+        if (!legacyInput) legacyInput = GetComponent<InputField>();
+#if TMP_PRESENT
+        if (!tmpInput) tmpInput = GetComponent<TMP_InputField>();
+#endif
     }
 
-    private void EnforceWordLimit(string text)
+    void OnEnable()
     {
-        string[] words = text.Split(new char[] { ' ', '\n', '\t' }, System.StringSplitOptions.RemoveEmptyEntries);
+        if (legacyInput) legacyInput.onValueChanged.AddListener(EnforceWordLimitLegacy);
+#if TMP_PRESENT
+        if (tmpInput) tmpInput.onValueChanged.AddListener(EnforceWordLimitTMP);
+#endif
+    }
+
+    void OnDisable()
+    {
+        if (legacyInput) legacyInput.onValueChanged.RemoveListener(EnforceWordLimitLegacy);
+#if TMP_PRESENT
+        if (tmpInput) tmpInput.onValueChanged.RemoveListener(EnforceWordLimitTMP);
+#endif
+    }
+
+    void EnforceWordLimitLegacy(string text)
+    {
+        var words = text.Split(new char[] { ' ', '\n', '\t' }, System.StringSplitOptions.RemoveEmptyEntries);
         if (words.Length > wordLimit)
         {
-            inputField.onValueChanged.RemoveListener(EnforceWordLimit);
-            inputField.text = string.Join(" ", words, 0, wordLimit);
-            inputField.onValueChanged.AddListener(EnforceWordLimit);
+            legacyInput.onValueChanged.RemoveListener(EnforceWordLimitLegacy);
+            legacyInput.text = string.Join(" ", words, 0, wordLimit);
+            legacyInput.onValueChanged.AddListener(EnforceWordLimitLegacy);
         }
     }
+
+#if TMP_PRESENT
+    void EnforceWordLimitTMP(string text)
+    {
+        var words = text.Split(new char[] { ' ', '\n', '\t' }, System.StringSplitOptions.RemoveEmptyEntries);
+        if (words.Length > wordLimit)
+        {
+            tmpInput.onValueChanged.RemoveListener(EnforceWordLimitTMP);
+            tmpInput.text = string.Join(" ", words, 0, wordLimit);
+            tmpInput.onValueChanged.AddListener(EnforceWordLimitTMP);
+        }
+    }
+#endif
 }
